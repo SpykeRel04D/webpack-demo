7fa737b1b769fbadc92867625fc8b7da493f1584
SpykeRel04D <roger.vidal.lloveras@gmail.com>
2021-02-10 03:08:47 +0100

    Adding our style files into webpack

--- readme.md
+++ readme.md
@@ -2,3 +2,7 @@ Webpack tutorial
 
 -> npm init -y
 -> npm install --save-dev webpack webpack-cli
+
+Then, to add loaders, pex:
+
+-> npm install --save-dev style-loader css-loader
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,24 @@
-import path from "path";
+const path = require('path');
 
 module.exports = {
-    devtool: "none",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+              },
+        ],
     },
 }
